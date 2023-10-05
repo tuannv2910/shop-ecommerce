e33ee9b851d334b060153b43092a64eafb3d08fd
tuan <tuannvph17655@gmail.co>
2023-10-05 23:26:52 +0700

    create entity and connect postgressql

---
+++ src/main/java/shop/ecommerce/common/contraints/enums/RequiredNote.java
@@ -0,0 +1,10 @@
+package shop.ecommerce.common.contraints.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RequiredNote {
+    CHOTHUHANG,
+    CHOXEMHANGKHONGTHU,
+    KHONGCHOXEMHANG;
+}
---
+++ src/main/java/shop/ecommerce/entites/OrderItem.java
@@ -0,0 +1,13 @@
+package shop.ecommerce.entites;
+
+public class OrderItem {
+    private String name;
+    private String code;
+    private Integer quantity;
+    private Long price;
+    private Long length;
+    private Long weight;
+    private Long width;
+    private Long height;
+    private Category category;
+}
